c0ce33528a5eb0278832c33b82ff6312287c9d24
Sarah Frainer <sbfrainer@gmail.com>
2024-04-25 13:05:12 -0300

    correção bugs

--- Exercita365/src/App.jsx
+++ Exercita365/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
+  return (
   <div>
 
 <Navbar />
@@ -12,7 +13,7 @@ function App() {
 
 <Footer />
 
-</div>  
+</div>  )
 }
 
 export default App
