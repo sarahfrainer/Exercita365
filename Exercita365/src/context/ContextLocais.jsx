e21791903f52b43c4568fe5f592eac19dc335144
Sarah Frainer <sbfrainer@gmail.com>
2024-04-25 13:38:17 -0300

    configuração do Context

--- Exercita365/src/context/ContextLocais.jsx
+++ Exercita365/src/context/ContextLocais.jsx
@@ -5,14 +5,28 @@ export const LocaisContext = createContext()
 export const LocaisContextProvider = ({children}) => {
     const [locais, setLocais] = useState ([
         {
-            ntrilha: "Trilha do macaco",
-            cidadeEstado: "Florianópolis - SC",
-            nusuario: "Sarah",
-            duracao: "2 horas",
-            trajeto: "6km",
-            dificuldade: "intermediário",
-            tipoDeTrilha: "Caminhada",
-            imagemTrilha: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            nlocal: "Trilha do macaco",
+            idusuario: "1234",
+            descrição: "Lindo lugar para fazer trilhas",
+            cep: "https://viacep.com.br/ws/88215000/json/",
+            coordenadas: "-27.17790080045851, -48.499666977182656",
+            tipoesporte: "trilha"
+        },
+        {
+            nlocal: "Praia do rosa",
+            idusuario: "1234",
+            descrição: "Belo mar, ondas calmas, perfeito para natação ao ar livre",
+            cep: "https://viacep.com.br/ws/88780-000/json/",
+            coordenadas: "-28.093553838175584, -48.63090678458072",
+            tipoesporte: "natação"
+        },
+        {
+            nlocal: "Beira-mar norte",
+            idusuario: "1234",
+            descrição: "Ótimo lugar para caminhadas e outros esportes",
+            cep: "https://viacep.com.br/ws/88015-702/json/",
+            coordenadas: "-27.56190094605664, -48.54074909040275",
+            tipoesporte: "caminhada"
         },
     ])
 
