95e4512f639acd94ba49d610c568bf4eb883c3c4
Sarah Frainer <sbfrainer@gmail.com>
2024-04-30 12:25:19 -0300

    estilização Navbar e Main

--- Exercita365/src/components/Navbar.jsx
+++ Exercita365/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useContext, useState } from 'react'
 import { UserContext } from '../context/UserContext';
+import styles from "./Navbar.module.css"
 
 export default function Navbar () {
 
@@ -8,12 +9,12 @@ export default function Navbar () {
 
     return (
 
-        <nav className="Navbar">
-            <Link to= "/" className= "GuiaDashboard">Página inicial</Link>
-            <Link to= "/login" className= "GuiaLogin">Login</Link>
-            <Link to= "/cadastro-local-exercicio" className= "GuiaCadastro">Cadastre novos locais</Link>
-            <Link to= "/lista-locais-exercicio" className= "GuiaLocais">Lista de locais</Link>
-            <button onClick={() => deslog()}>Sair</button>
+        <nav className={styles.Navbar}>
+            <Link to= "/" className={styles.GuiaDashboard}>Página inicial</Link>
+            <Link to= "/login" className={styles.GuiaLogin}>Login</Link>
+            <Link to= "/cadastro-local-exercicio" className={styles.GuiaCadastro}>Cadastre novos locais</Link>
+            <Link to= "/lista-locais-exercicio" className={styles.GuiaLocais}>Lista de locais</Link>
+            <div className={styles.Sair} onClick={() => deslog()}>Sair</div>
         </nav>
     )
 }
